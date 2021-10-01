0919f8fd1976a7f17d612f38dc5033d957b9a380
Wendy <wendy.zeng@shoalter.com>
2021-10-01 17:27:49 +0800

    [fix] 下拉更新

--- app/src/main/java/com/test/randomcolor/MainActivity.java
+++ app/src/main/java/com/test/randomcolor/MainActivity.java
@@ -1,6 +1,5 @@
 package com.test.randomcolor;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -16,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
 
@@ -45,21 +45,12 @@ public class MainActivity extends AppCompatActivity {
         // size
         swipeRefreshLayout.setSize(SwipeRefreshLayout.LARGE);
         // 監聽下拉刷新
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        link.clear();
-                        addColor();
-                        myAdapter.notifyItemRangeChanged(0,30);
-                        swipeRefreshLayout.setRefreshing(false);
-                    }
-                }, 1000);
-
-            }
-        });
+        swipeRefreshLayout.setOnRefreshListener(() -> new Handler().postDelayed(() -> {
+            link.clear();
+            addColor();
+            myAdapter.notifyDataSetChanged();
+            swipeRefreshLayout.setRefreshing(false);
+        }, 1000));
 
 
         //設置RecycleView
@@ -69,20 +60,17 @@ public class MainActivity extends AppCompatActivity {
 
         //設置初始資料
         addColor();
-        mRecyclerViewUtil=new RecyclerViewUtil(recyclerView);
-        mRecyclerViewUtil.setRecyclerViewLoadMoreListener(new RecyclerViewUtil.RecyclerViewLoadMoreListener() {
-            @Override
-            public void onLoadMore() {
-                Toast.makeText(getApplicationContext(),"已經到底，加載中",Toast.LENGTH_SHORT).show();
-                mRecyclerViewUtil.setLoadMoreEnable(false);
-                load();
-                Log.d(TAG, String.valueOf(link.size()));
-            }
+        mRecyclerViewUtil = new RecyclerViewUtil(recyclerView);
+        mRecyclerViewUtil.setRecyclerViewLoadMoreListener(() -> {
+            Toast.makeText(getApplicationContext(), "已經到底，加載中", Toast.LENGTH_SHORT).show();
+            mRecyclerViewUtil.setLoadMoreEnable(false);
+            load();
+            Log.d(TAG, String.valueOf(link.size()));
         });
     }
 
 
-    public void addColor(){
+    public void addColor() {
         for (int i = 0; i < 30; i++) {
             color = getColor();
             link.add("https://dummyimage.com/300x300/" + color + "/ffffff&text=" + color);
@@ -90,12 +78,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void load() {
-        if (link.size()<=270){
+        if (link.size() <= 270) {
             addColor();
-            myAdapter.notifyItemRangeInserted(link.size()-30,30);
+            myAdapter.notifyItemRangeInserted(link.size() - 30, 30);
             Toast.makeText(getApplicationContext(), "加載了" + 30 + "條資料", Toast.LENGTH_SHORT).show();
             mRecyclerViewUtil.setLoadMoreEnable(true);
-        }else{
+        } else {
             Toast.makeText(getApplicationContext(), "已無最新資料", Toast.LENGTH_SHORT).show();
             mRecyclerViewUtil.setLoadMoreEnable(false);
         }
@@ -103,30 +91,25 @@ public class MainActivity extends AppCompatActivity {
 
 
     public String getColor() {
-        String str = "";
+        StringBuilder str = new StringBuilder();
         //一個十六進位制的值得陣列
-        String arr[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+        String[] arr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 
         for (int i = 0; i < 6; i++) {
             //產生的每個隨機數都是一個索引，根據索引找到陣列中對應的值，拼接到一起
             int num = (int) (Math.random() * 16);
-            str += arr[num];
+            str.append(arr[num]);
         }
 
-        int test=(int)(Math.random()*16777216);
-        String hex=Integer.toHexString(test);
+        int test = (int) (Math.random() * 16777216);
+        String hex = Integer.toHexString(test);
         Log.d(TAG, hex);
 
-        return str;
+        return str.toString();
     }
 
 
     private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-
-        @SuppressLint("NotifyDataSetChanged")
-        public void refreshList() {
-            notifyDataSetChanged();
-        }
 
         @NonNull
         @Override
